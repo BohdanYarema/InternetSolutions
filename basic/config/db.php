2050b4f6e4c177d8247fd3889a77ec2fd0e2310d
Bohdan Yarema <bohdanyarema1992@gmail.com>
2015-11-19 13:58:41 +0200

    Dell all test info

--- basic/config/db.php
+++ basic/config/db.php
@@ -7,3 +7,12 @@ return [
     'password' => '',
     'charset' => 'utf8',
 ];
+
+/*return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=park_b2b',
+    'username' => 'park_b2b',
+    'password' => 'a2jtkpmv',
+    'charset' => 'utf8',
+];*/
+
